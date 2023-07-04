af81128e3bb6d1f283c9b09996f6978dcb743774
Kezara Lakshan <kezaralakshan666@gmail.com>
2023-07-04 08:08:33 +0530

    Add existing project files to Git

---
+++ vidly/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace vidly.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public double price { get; set; }
+        
+        public String description { get; set; }
+
+
+        [JsonPropertyName("image")]
+        public String image { get; set; }
+
+        public override string ToString()=>JsonSerializer.Serialize<Product>(this);
+        
+    }
+}
---
+++ vidly/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using vidly.Models;
+
+namespace vidly.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService _productService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger,JsonFileProductService jsonFileProductService)
+        {
+            _logger = logger;
+            _productService = jsonFileProductService;
+        }
+
+        public void OnGet()
+           {
+            Products = _productService.GetProducts();
+
+        }
+    }
+}
